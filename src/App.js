8f8647b23b537aa0f3835e1f2fc967f19cad7dd2
Amritbarnwal <amritbarnwal2106@gmail.com>
2024-09-25 19:20:16 +0530

    test_commmit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./Components/TicTacToe/TicTacToe";
+import TicTacToe from "./Components/TicTacToe/TicTacToe";
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+      
+    </div>
+  );
+}
+
+export default App;
